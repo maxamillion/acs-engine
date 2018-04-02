5253b1368d16e436d7cf5ed92748497cc75ce8c0
Jim Minter <jminter@redhat.com>
2018-04-02 18:02:14 -0500

    add AzProfile

--- pkg/acsengine/defaults.go
+++ pkg/acsengine/defaults.go
@@ -275,7 +275,7 @@ func SetPropertiesDefaults(cs *api.ContainerService, isUpgrade bool) (bool, erro
 	setStorageDefaults(properties)
 	setExtensionDefaults(properties)
 
-	certsGenerated, e := setDefaultCerts(properties, cs.Location)
+	certsGenerated, e := setDefaultCerts(properties)
 	if e != nil {
 		return false, e
 	}
@@ -660,9 +660,9 @@ func setStorageDefaults(a *api.Properties) {
 	}
 }
 
-func openShiftSetDefaultCerts(a *api.Properties, location string) (bool, error) {
-	externalMasterHostname := fmt.Sprintf("%s.%s.cloudapp.azure.com", a.MasterProfile.DNSPrefix, location)
-	routerLBHostname := fmt.Sprintf("%s-router.%s.cloudapp.azure.com", a.MasterProfile.DNSPrefix, location)
+func openShiftSetDefaultCerts(a *api.Properties) (bool, error) {
+	externalMasterHostname := fmt.Sprintf("%s.%s.cloudapp.azure.com", a.MasterProfile.DNSPrefix, a.AzProfile.Location)
+	routerLBHostname := fmt.Sprintf("%s-router.%s.cloudapp.azure.com", a.MasterProfile.DNSPrefix, a.AzProfile.Location)
 	c := certgen.Config{
 		Master: &certgen.Master{
 			Hostname: fmt.Sprintf("%s-master-%s-0", DefaultOpenshiftOrchestratorName, GenerateClusterID(a)),
@@ -738,9 +738,9 @@ func getConfigBundle(write writeFn) ([]byte, error) {
 	return b.Bytes(), nil
 }
 
-func setDefaultCerts(a *api.Properties, location string) (bool, error) {
+func setDefaultCerts(a *api.Properties) (bool, error) {
 	if a.MasterProfile != nil && a.OrchestratorProfile.OrchestratorType == api.OpenShift {
-		return openShiftSetDefaultCerts(a, location)
+		return openShiftSetDefaultCerts(a)
 	}
 
 	if a.MasterProfile == nil || a.OrchestratorProfile.OrchestratorType != api.Kubernetes {
